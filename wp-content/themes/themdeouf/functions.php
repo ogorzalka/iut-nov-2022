8b881d0f300c1998a542c3442ae899a8f0b7a990
Olivier Gorzalka <olivier@amphibee.fr>
2022-11-18 14:16:33 +0100

    Ajout dynamisation de la récupération de contenu

--- wp-content/themes/themdeouf/functions.php
+++ wp-content/themes/themdeouf/functions.php
@@ -1,3 +1,10 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 require dirname(__FILE__) . DS . 'inc' . DS . 'assets.php';
+
+//add_action('tdo/mon_hook', function($title, $content) {
+//    echo "<h1>{$title}</h1>";
+//    echo "<p>{$content}</p>";
+//}, 10, 2);
+
+
--- wp-content/themes/themdeouf/index.php
+++ wp-content/themes/themdeouf/index.php
@@ -1,3 +1,13 @@
 <?php get_template_part('partials/head'); ?>
-    <h1>Test</h1>
+    <?php
+        // do_action('tdo/mon_hook', 'Mon titre', 'mon contenu');
+    ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <article>
+            <h1><?php the_title(); ?></h1>
+            <div>
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; ?>
 <?php get_template_part('partials/foot') ?>
