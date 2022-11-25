bd3d34a4a6a81eec0292766e0e04a05d35bc5866
Olivier Gorzalka <olivier@amphibee.fr>
2022-11-25 13:54:50 +0100

    Template indépendant pour le formulaire

--- wp-content/themes/themdeouf/inc/fields/project.php
+++ wp-content/themes/themdeouf/inc/fields/project.php
@@ -3,18 +3,10 @@ add_action( 'add_meta_boxes', function() {
     add_meta_box(
         'project-datas',
         'Informations additionnelles projet',
-        'project_render_meta_box',
+        function($post) {
+            get_template_part('admin/metaboxes/project', 'settings');
+        },
         ['portfolio'],
         'side'
     );
 });
-
-function project_render_meta_box() {
-    ?>
-    <div class="project-meta-box">
-        <div class="project-meta-box-head">
-            <p>Veuillez renseigner les champs ci-dessous</p>
-        </div>
-    </div>
-    <?php
-}
